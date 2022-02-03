6484fa1bc477e1b5feb753b99f84ea811e5d6844
vernfongchao <Vernchao93@gmail.com>
2022-02-03 10:27:16 -0800

    auth almost done

---
+++ backend/db/migrations/20220203172355-create-user.js
@@ -0,0 +1,41 @@
+'use strict';
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable('Users', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      username: {
+        allowNull: false,
+        unique: true,
+        type: Sequelize.STRING(30)
+      },
+      email: {
+        allowNull: false,
+        unique: true,
+        type: Sequelize.STRING(256)
+      },
+      hashedPassword: {
+        allowNull: false,
+        type: Sequelize.STRING.BINARY
+      },
+      createdAt: {
+        allowNull: false,
+        defaultValue: Sequelize.fn('now'),
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        defaultValue: Sequelize.fn('now'),
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.dropTable('Users');
+  }
+};
--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -1,9 +1,31 @@
 const router = require('express').Router();
+const asyncHandler = require('express-async-handler');
+const { User } = require('../../db/models');
+const { setTokenCookie, restoreUser, requireAuth } = require('../../utils/auth.js');
+const sessionRouter = require('./session')
+const usersRouter = require('./user')
 
-//testing
-router.post('/test', function(req, res) {
+router.use('/session', sessionRouter);
+
+router.use('/users', usersRouter);
+
+
+router.get('/set-token-cookie', asyncHandler(async (_req, res) => {
+    const user = await User.findOne({
+        where: {
+            username: 'Demo-lition'
+        }
+    });
+    setTokenCookie(res, user);
+    return res.json({ user });
+}));
+
+
+router.post('/test', (req, res) => {
     res.json({ requestBody: req.body });
-  });
+});
+
+
 
 
 module.exports = router;
