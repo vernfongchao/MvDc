0f974f113a1d39b48904c12149df6f1072a6dd8e
vernfongchao <Vernchao93@gmail.com>
2022-02-03 12:48:19 -0800

    set up frontend token fetch

--- backend/routes/api/session.js
+++ backend/routes/api/session.js
@@ -1,10 +1,22 @@
 const express = require('express');
 const asyncHandler = require('express-async-handler');
-
 const { setTokenCookie, restoreUser } = require('../../utils/auth');
+const { check } = require('express-validator');
+const { handleValidationErrors } = require('../../utils/validation');
 const { User } = require('../../db/models');
 
 const router = express.Router();
+
+const validateLogin = [
+    check('credential')
+        .exists({ checkFalsy: true })
+        .notEmpty()
+        .withMessage('Please provide a valid email or username.'),
+    check('password')
+        .exists({ checkFalsy: true })
+        .withMessage('Please provide a password.'),
+    handleValidationErrors
+];
 
 router.get(
     '/',
@@ -21,6 +33,7 @@ router.get(
 
 router.post(
     '/',
+    validateLogin,
     asyncHandler(async (req, res, next) => {
         const { credential, password } = req.body;
 
