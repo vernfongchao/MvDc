504540c0f39f28759863debe6af4347bb7566011
vernfongchao <Vernchao93@gmail.com>
2022-02-12 11:45:39 -0800

    seearch bar and search page set up

--- frontend/src/components/SearchBar/SearchPage.js
+++ frontend/src/components/SearchBar/SearchPage.js
@@ -1,10 +1,40 @@
-import React from "react"
+import React, { useEffect } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { Link, useLocation } from 'react-router-dom';
+import { getQuestions } from '../../store/question';
+
+
 
 const SearchPage = () => {
+    const dispatch = useDispatch
+    const questions = useSelector((state) => state.questionState?.questions)
+
+    useEffect(() => {
+        dispatch(getQuestions())
+    }, [dispatch]);
+
+
+
+    const questionsArr = Object.values(questions)
+
+    const location = useLocation()
+
+
+
+    console.log('======================', location.state.detail)
+
+
     return (
         <div>
-            <p>fgsdfvbljkxcvbljkhsdjklfhgh jklsdfghjk sdfhjg hjsdfghjk sdhjklfg shjkdfghjklsdjklfg</p>
-            </div>
+            {questionsArr?.map(({ title, id }) => (
+                <Link to={`questions/${id}`} key={id}>
+                    <p>
+                        {title}
+                    </p>
+
+                </Link>
+            ))}
+        </div>
     )
 }
 
